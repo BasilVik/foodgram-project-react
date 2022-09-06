a811ad9b200eddc76d325713b134a3e9f5872c6f
vasiliy <basilvik>
2022-09-06 12:52:04 +0700

    create user and follow model

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name',)
+
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Адрес электронной почты')
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Уникальный юзернейм'
+    )
+
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'Пользователь {self.username}'
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Подписчик',
+        on_delete=models.CASCADE,
+        related_name='follower',
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.author}'

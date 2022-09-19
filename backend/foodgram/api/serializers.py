4d1a7121ba416bf2b732c801e4a617d9452c35b6
vasiliy <basilvik>
2022-09-19 11:16:07 +0700

    fix serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -87,8 +87,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        ingredients = validated_data.get('ingredients')
-        # validate_ingredient_amounts(ingredients)
+        ingredients = self.initial_data.get('ingredients')
+        # ingredients = validated_data.get('ingredients')
+        validate_ingredient_amounts(ingredients)
         super().update(instance, validated_data)
         instance.tags.clear()
         instance.tags.set(validated_data.pop('tags'))
